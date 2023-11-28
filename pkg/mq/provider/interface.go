d3bcf1a585ba7fa521bde8c3008cb33593d1dd56
Jos-bases <142135994+Jos-bases@users.noreply.github.com>
2023-11-28 11:13:55 +0800

    Update interface.go

--- pkg/mq/provider/interface.go
+++ pkg/mq/provider/interface.go
@@ -1,7 +1,6 @@
 package provider
 
 import (
-	amqp "github.com/rabbitmq/amqp091-go"
 	"github.com/Jos-bases/toolMq/pkg/mq/conf"
 )
 
@@ -12,8 +11,10 @@ type MqFace interface {
 	DeleteQueue(queue *conf.Queue) error
 	QueueBind(key string, exchange *conf.Exchange, queue *conf.Queue) error
 	QueueUnBind(key string, exchange *conf.Exchange, queue *conf.Queue) error
-	Subscribe(key string, queue *conf.Queue, callback func(<-chan amqp.Delivery, string))
+	Subscribe(subscribe *conf.Subscribe, queue *conf.Queue, callback func(<-chan MqDelivery))
 	SendMessage(key string, exchange *conf.Exchange, date []byte) error
+	Ack(tag uint64, multiple bool) error
+	Nack(tag uint64, multiple bool, requeue bool) error
 	Close() error
 	ChannelClose() error
 }
