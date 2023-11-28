01b9637322734eec21750c59f4e4cd2fc651c82b
Jos-bases <142135994+Jos-bases@users.noreply.github.com>
2023-11-28 11:12:18 +0800

    Update exchange.go

--- pkg/mq/conf/exchange.go
+++ pkg/mq/conf/exchange.go
@@ -10,10 +10,13 @@ const (
 )
 
 type Exchange struct {
-	Name       string    `json:"name"`
-	Types      ExchangeT `json:"types"`
-	Durable    bool      `json:"durable"`
-	AutoDelete bool      `json:"auto_delete"`
-	Internal   bool      `json:"internal"`
-	NoWait     bool      `json:"no_wait"`
+	Name       string                 `json:"name"`
+	Types      ExchangeT              `json:"types"`
+	Durable    bool                   `json:"durable"`
+	AutoDelete bool                   `json:"auto_delete"`
+	Internal   bool                   `json:"internal"`
+	NoWait     bool                   `json:"no_wait"`
+	IfUnused   bool                   `json:"if_unused"`
+	IfEmpty    bool                   `json:"if_empty"`
+	Args       map[string]interface{} `json:"args"`
 }
